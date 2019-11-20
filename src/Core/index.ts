14eaf9fc744ac050b7c64322daaaeec010fb9b11
Mihailescu Marian <mariandev96@gmail.com>
2019-11-20 17:44:20 +0200

    chore: Restructure

--- src/Core/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Conditions";
-export * from "./Entity";
-export * from "./Component";
-export * from "./World";
-export * from "./System";
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export * from "./Core";
+export * from "./Conditions";
+export * from "./Entity";
+export * from "./Component";
+export * from "./World";
+export * from "./System";
